dc848e54a6d33360b34f40faf8389407e079bd91
Jasson Rodriguez <jassonrm@icloud.com>
2022-11-13 01:24:14 -0600

    Connect Windows notifications with the sound

--- utility.py
+++ utility.py
@@ -1,10 +1,22 @@
 import keyboard
 import serial
+from winsdk.windows.ui.notifications.management import UserNotificationListener, UserNotificationListenerAccessStatus
+from winsdk.windows.ui.notifications import NotificationKinds, KnownNotificationBindings
 
 port = "COM3"
 baudrate = 9600
 
 device = serial.Serial(port, baudrate, timeout=1)
+
+def handler(listener, event):
+    notification = listener.get_notification(event.user_notification_id)
+
+    # Do this to avoid double notification
+    if hasattr(notification, "app_info"):
+        device.write("N".encode("utf-8"))
+
+listener = UserNotificationListener.get_current()
+listener.add_notification_changed(handler)   
 
 while True:
     input = device.readline().decode("utf-8")
@@ -14,5 +26,6 @@ while True:
             keyboard.press_and_release("ctrl+c")
         elif (code == 2):
             keyboard.press_and_release("ctrl+v")
-        else:
-            device.write("N".encode("utf-8"))
+
+
+
